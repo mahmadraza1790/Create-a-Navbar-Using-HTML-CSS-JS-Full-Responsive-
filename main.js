6f5fbc1e10ad7bf32e199cef46bb9cb07c669de6
Muhammad Ahmad Raza <muhammadahmadraza1790@gmail.com>
2024-07-03 02:00:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.getElementById("menu-btn");
+const navlinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+  navlinks.classList.toggle("open");
+  
+  const isOpen = navlinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen? "ri-close-line" : "ri-menu-line");
+});
+
+navlinks.addEventListener("click", () => {
+    navlinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+  });
